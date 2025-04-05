1c97ba7420fef9eedc9c254684ed7bf58e55e408
Yalikun Yilida <eldar940818@gmail.com>
2025-04-05 16:26:27 +0200

    add essential pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
 from data import products_list
 
 app = Flask(__name__)
@@ -10,6 +10,14 @@ def home():
 @app.route("/create")
 def create():
     return render_template("create.html")
+
+@app.route("/product/<string:pid>")
+def product_info(pid):
+    product = next((pd for pd in products_list if pd["id"] == pid), None)
+    if product is None:
+        abort(404)
+        
+    return render_template("information.html", product=product)
 
 if __name__ == "__main__":
     app.run(debug=True)
